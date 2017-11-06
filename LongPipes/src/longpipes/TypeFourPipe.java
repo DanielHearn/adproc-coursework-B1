0565fd53c4e5fa82e98b7bcbca91bf14b5f2c9bd
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-11-06 09:42:19 +0000

    Fix class naming

---
+++ LongPipes/src/longpipes/TypeFourPipe.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longpipes;
+
+/**
+ *
+ * @author up801685
+ */
+public class TypeFourPipe extends Pipe{
+    
+    public TypeFourPipe(double inputLength, double inputDiameter) {
+        super(inputLength, inputDiameter);
+    }
+    
+}
---
+++ LongPipes/src/longpipes/TypeThreePipe.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longpipes;
+
+/**
+ *
+ * @author up801685
+ */
+public class TypeThreePipe extends Pipe{
+    
+    public TypeThreePipe(double inputLength, double inputDiameter) {
+        super(inputLength, inputDiameter);
+    }
+    
+}
