fe5b8003688275bfaab09ef45cdf85be7b325758
Liam Kerrigan <kerrjgan@outlook.com>
2017-11-17 13:34:13 +0000

    Created final order form. Started wiring it up.

---
+++ LongPipes/src/longpipes/LongPipesGUILink.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longpipes;
+
+/**
+ *
+ * @author up750834
+ */
+public class LongPipesGUILink {
+    
+    static Order order = new Order();
+    
+    // Variables to pull from GUI
+    public double PipeLength;
+    public double PipeDiameter;
+    public int PipeGrade;
+    public int PipeColor;
+    public int PipeQuantity;
+    public Boolean PipeInsulation;
+    public Boolean PipeRefinforcement;
+    public Boolean PipeChemicalResistance;
+   
+    
+    public LongPipesGUILink() {
+        
+    }
+    
+}
